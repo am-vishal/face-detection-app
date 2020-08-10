9dcb9eb8d7b132fd3765377fcd6ea449b20b9689
Vishal Kumar <vishal.k0905@gmail.com>
2020-08-10 23:24:49 +0530

    Updates

---
+++ precache-manifest.2bcb16ec095c3450d922339f2d92ca08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b1879a86a444ca1e43398246daa50c6",
+    "url": "/am-vishal/face-detection-app/index.html"
+  },
+  {
+    "revision": "7264f864d569665b114b",
+    "url": "/am-vishal/face-detection-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7e95f28dafd6d9b7143b",
+    "url": "/am-vishal/face-detection-app/static/css/main.44c59957.chunk.css"
+  },
+  {
+    "revision": "7264f864d569665b114b",
+    "url": "/am-vishal/face-detection-app/static/js/2.b0c0ecaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/am-vishal/face-detection-app/static/js/2.b0c0ecaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e95f28dafd6d9b7143b",
+    "url": "/am-vishal/face-detection-app/static/js/main.5e11ad75.chunk.js"
+  },
+  {
+    "revision": "c9183bcf372d178d97e3",
+    "url": "/am-vishal/face-detection-app/static/js/runtime-main.08bd1a54.js"
+  }
+]);
